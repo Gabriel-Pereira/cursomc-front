fa50a60a28e0924f1d8103deea859eb59cec6c99
Gabriel-Pereira <gabrielprra@hotmail.com>
2020-01-14 22:39:56 -0300

    Terminando as funcionalidades do carrinho

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -4,16 +4,18 @@ import { API_CONFIG } from "../config/api.config";
 import { CredenciaisDTO } from "../models/credenciais.dt";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
-import { JwtHelper } from 'angular2-jwt';
+import { JwtHelper } from "angular2-jwt";
+import { CartService } from "./domain/cart.service";
 
 @Injectable()
 export class AuthService {
-
   jwtHelper: JwtHelper = new JwtHelper();
 
-  constructor(public http: HttpClient, public storage: StorageService) {
-      
-  }
+  constructor(
+    public http: HttpClient,
+    public storage: StorageService,
+    public cartService: CartService
+  ) {}
 
   authenticate(creds: CredenciaisDTO) {
     return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
@@ -24,24 +26,26 @@ export class AuthService {
 
   refreshToken() {
     return this.http.post(
-        `${API_CONFIG.baseUrl}/auth/refresh_token`, 
-        {},
-        {
-            observe: 'response',
-            responseType: 'text'
-        });
-}
+      `${API_CONFIG.baseUrl}/auth/refresh_token`,
+      {},
+      {
+        observe: "response",
+        responseType: "text"
+      }
+    );
+  }
 
-  successfulLogin(authorizationValue : string) {
+  successfulLogin(authorizationValue: string) {
     let tok = authorizationValue.substring(7);
-    let user : LocalUser = {
-        token: tok,
-        email: this.jwtHelper.decodeToken(tok).sub
+    let user: LocalUser = {
+      token: tok,
+      email: this.jwtHelper.decodeToken(tok).sub
     };
     this.storage.setLocalUser(user);
-}
+    this.cartService.createOrClearCart();
+  }
 
-logout() {
+  logout() {
     this.storage.setLocalUser(null);
-}
+  }
 }
